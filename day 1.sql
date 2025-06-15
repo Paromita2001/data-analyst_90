2b87dbd3b2fd23399c3ba631a142ebc59e619798
Paromita karmakar <137789638+Paromita2001@users.noreply.github.com>
2025-06-15 23:09:04 +0530

    Add files via upload

--- day 1.sql
+++ day 1.sql
@@ -1,34 +1,30 @@
 create database employee_details
 
 
-CREATE TABLE Employees (
+CREATE TABLE EmployeeRecords (
     EmployeeID INT PRIMARY KEY,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Department VARCHAR(50),
-    Salary DECIMAL(10, 2),
-    HireDate DATE
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    Department NVARCHAR(50),
+    Salary DECIMAL(10, 2)
 );
 
 
-
-INSERT INTO Employees (EmployeeID, FirstName, LastName, Department, Salary, HireDate)
+INSERT INTO EmployeeRecords (EmployeeID, FirstName, LastName, Department, Salary)
 VALUES
-(1, 'John', 'Doe', 'HR', 50000.00, '2020-01-15'),
-(2, 'Jane', 'Smith', 'Finance', 60000.00, '2019-03-23'),
-(3, 'Alice', 'Johnson', 'IT', 75000.00, '2021-07-10'),
-(4, 'Bob', 'Brown', 'Marketing', 55000.00, '2018-09-30'),
-(5, 'Charlie', 'Davis', 'IT', 70000.00, '2022-02-20');
+(1, 'John', 'Miller', 'HR', 60000),
+(2, 'Jane', 'Smith', 'Finance', 75000),
+(3, 'John', 'Miller', 'HR', 60000),
+(4, 'Emily', 'Davis', 'IT', 85000),
+(5, 'Michael', 'Brown', 'Finance', 75000),
+(6, 'Jane', 'Smith', 'Finance', 75000);
 
-SELECT * FROM Employees
+select * from employeerecords
 
-select firstname from Employees
+select distinct firstname from employeerecords
 
-select firstname,Salary from Employees
+select distinct firstname,lastname from employeerecords
 
-select FirstName,LastName from Employees
+select distinct salary from employeerecords
 
-select EmployeeID,concat(firstname,' ',LastName) from Employees
-
-
-select EmployeeID,concat(firstname,' ',LastName) as Full_Name from Employees
+select distinct * from employeerecords
