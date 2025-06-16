41804924a5bfc90a1acca479720f48894f314002
Paromita karmakar <137789638+Paromita2001@users.noreply.github.com>
2025-06-16 18:22:43 +0530

    Add files via upload

---
+++ day2_and_or.sql
@@ -0,0 +1,13 @@
+
+select * from EmployeeRecords
+where LastName = 'Miller' and EmployeeID = 3
+
+select * from EmployeeRecords
+where LastName = 'Miller' and EmployeeID = '3'
+
+select * from EmployeeRecords
+where Department = 'HR' or Department = 'Finance'
+
+select * from  EmployeeRecords 
+where (Department = 'HR' or Department = 'Finance') 
+and Salary = 60000
