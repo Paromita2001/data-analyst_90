83b101afac0f2db1087077ac53e8a5bbf37eba9f
Paromita karmakar <137789638+Paromita2001@users.noreply.github.com>
2025-06-16 18:25:52 +0530

    Add files via upload

---
+++ day2_orderby.sql
@@ -0,0 +1,13 @@
+select * from employees
+
+select * from employees 
+order by salary 
+
+select * from employees 
+order by salary desc
+
+select * from employees 
+order by firstname asc, salary desc
+
+select * from employees 
+order by department asc, salary desc
