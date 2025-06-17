a965609711572db9dd57fb041049ebbc6f65c78b
Paromita karmakar <137789638+Paromita2001@users.noreply.github.com>
2025-06-17 16:32:22 +0530

    Add files via upload

---
+++ day3_insert_into.sql
@@ -0,0 +1,28 @@
+
+select * from Employees
+
+insert into Employees (EmployeeID,FirstName,LastName,Department,Salary,HireDate)
+values (6,'Raj','Ambani','IT',67000,'2023-04-20')
+
+INSERT INTO Employees (EmployeeID,FirstName,LastName)
+VALUES (7,'Rohit','Mehera')
+
+insert into Employees
+values (8,'Mahesh','Narang','HR',73000,'2024-01-21')
+
+SELECT * FROM Employees
+
+
+
+insert into Employees
+values (9,'Jay','','IT',73000,'2022-04-04')
+
+
+INSERT INTO Employees
+VALUES (10,'Nitin','shamani','0',54000,'2021-02-22')
+
+select * from Employees where Department = null
+
+select * from Employees where Department is null
+
+select * from Employees where Department is not null
